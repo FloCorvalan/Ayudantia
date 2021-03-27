3d25e44d07efd149e431d182e82d8d00c82f6d8a
Florencia <florencia.corvalan@usach.cl>
2021-03-27 15:53:50 -0300

    Cambios en Dockerfiles de front y back y vuelta a localhost en Registro.vue

--- front/src/views/Registro.vue
+++ front/src/views/Registro.vue
@@ -56,7 +56,7 @@ export default {
   methods: {
     calcularSuma: function () {
       axios
-        .post("http://backend.default.svc.cluster.local:30000/registro", {
+        .post("http://localhost:30000/registro", {
           operando1: this.operando1,
           operando2: this.operando2,
           nombre: this.nombre,
