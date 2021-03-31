d5b833af26a8accd37af129199b6ee95cbe60ea3
Florencia <florencia.corvalan@usach.cl>
2021-03-30 22:33:18 -0300

    Cambios en vue.config.js y listen 8080 en nginx.conf

--- front/vue.config.js
+++ front/vue.config.js
@@ -2,4 +2,9 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'build.js',
+    publicPath: './static'
+}
 }
