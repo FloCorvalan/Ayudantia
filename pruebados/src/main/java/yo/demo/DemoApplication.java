b30e6a8331c9454127516889d1e20e274d99628b
Florencia <florencia.corvalan@usach.cl>
2021-04-06 23:00:23 -0400

    Modificación en DemoApplication.java del back

--- pruebados/src/main/java/yo/demo/DemoApplication.java
+++ pruebados/src/main/java/yo/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package yo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
